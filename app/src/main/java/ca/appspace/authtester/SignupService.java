7add9fbaba407125802e65c26bc6ea1b5c6fd4dd
Eugene <esukharev@casebank.com>
2014-10-24 18:10:09 -0400

    rECENT CHANGES

--- app/src/main/java/ca/appspace/authtester/SignupService.java
+++ app/src/main/java/ca/appspace/authtester/SignupService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import retrofit.RestAdapter;
+
 public class SignupService extends Service {
 
     public SignupService() {
@@ -12,5 +14,18 @@ public class SignupService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private void sendTestSignup() {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://127.0.0.1")
+                .build();
+        RemoteSignupService remoteSignupService = restAdapter.create(RemoteSignupService.class);
+        SignupData data = new SignupData();
+        data.setEmail("eugene@appspace.ca");
+        data.setName("Eugene");
+        data.setDoctor(true);
+        data.setMedicalRecord("123123123");
+        remoteSignupService.signUp(data);
     }
 }
